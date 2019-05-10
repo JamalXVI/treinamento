a25b2006b390bf021412bcca77823fc7b2474572
JamalXVI <henriquearantest@gmail.com>
2019-05-10 11:34:52 -0300

    Sobe Implementacoes

--- src/main/java/br/com/socin/treinamento/primeiro/service/impl/CartaServiceImpl.java
+++ src/main/java/br/com/socin/treinamento/primeiro/service/impl/CartaServiceImpl.java
@@ -1,17 +1,19 @@
 package br.com.socin.treinamento.primeiro.service.impl;
 
-import br.com.socin.treinamento.primeiro.enums.Naipe;
-import br.com.socin.treinamento.primeiro.model.Carta;
-import br.com.socin.treinamento.primeiro.repository.CartaRepository;
-import br.com.socin.treinamento.primeiro.repository.JogadorRepository;
-import br.com.socin.treinamento.primeiro.service.CartaService;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Map;
+import br.com.socin.treinamento.primeiro.enums.Naipe;
+import br.com.socin.treinamento.primeiro.model.Carta;
+import br.com.socin.treinamento.primeiro.model.Jogador;
+import br.com.socin.treinamento.primeiro.model.Jogo;
+import br.com.socin.treinamento.primeiro.repository.CartaRepository;
+import br.com.socin.treinamento.primeiro.service.CartaService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Service
 @AllArgsConstructor
@@ -20,11 +22,13 @@ public class CartaServiceImpl implements CartaService {
   @Autowired
   CartaRepository cartaRepository;
 
-  @Autowired
-  JogadorRepository jogadorRepository;
-
   @Override
   public Map<Naipe, List<Carta>> ordernarCartasPorNaipe() {
+    return null;
+  }
+
+  @Override
+  public List<Carta> entregaCartasParaJogador(Jogador jogador, Jogo jogo) {
     return null;
   }
 
--- src/main/java/br/com/socin/treinamento/primeiro/service/impl/JogoServiceImpl.java
+++ src/main/java/br/com/socin/treinamento/primeiro/service/impl/JogoServiceImpl.java
@@ -1,21 +1,28 @@
 package br.com.socin.treinamento.primeiro.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import br.com.socin.treinamento.primeiro.enums.TipoDeJogo;
+import br.com.socin.treinamento.primeiro.model.Jogo;
 import br.com.socin.treinamento.primeiro.repository.JogoRepository;
 import br.com.socin.treinamento.primeiro.service.CartaService;
 import br.com.socin.treinamento.primeiro.service.JogadorService;
 import br.com.socin.treinamento.primeiro.service.JogoService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @AllArgsConstructor
 @NoArgsConstructor
 public class JogoServiceImpl implements JogoService {
+  public static final String TAMANHO_LISTA_IGUAL = "O tamanho da lista deve ser o mesmo";
+  public static final String O_LIMITE_DO_TIPO_JOGO_DEVE_SER_MAIOR_OU_IGUAL =
+      "O limite do tipoJogo deve ser maior ou igual";
+  public static final String O_JOGO_NÃO_PODE_ESTAR_ENCERRADO = "O jogo não pode estar encerrado";
+  public static final String O_JOGO_DEVE_EXISTIR = "O jogo deve existir";
   @Autowired
   private JogoRepository jogoRepository;
   @Autowired
@@ -24,13 +31,17 @@ public class JogoServiceImpl implements JogoService {
   private CartaService cartaService;
 
   @Override
-  public Integer iniciarJogo(List<Integer> jogadores, TipoDeJogo jogo) {
-
+  public Jogo iniciarJogo(List<Integer> jogadores, List<Integer> ordem, TipoDeJogo tipoJogo) {
     return null;
   }
 
   @Override
-  public Boolean distribuiCartas(Integer idJogo) {
+  public Jogo distribuiCartas(Integer idJogo) {
     return null;
+  }
+
+  @Override
+  public Jogo salvar(Jogo jogo) {
+    return jogoRepository.save(jogo);
   }
 }
