035d6bed8826469a79e09f2e892af4321c164980
JamalXVI <henriquearantest@gmail.com>
2019-05-31 17:39:39 -0300

    fim do curso

--- src/main/java/br/com/socin/treinamento/primeiro/service/impl/CartaServiceImpl.java
+++ src/main/java/br/com/socin/treinamento/primeiro/service/impl/CartaServiceImpl.java
@@ -1,12 +1,17 @@
 package br.com.socin.treinamento.primeiro.service.impl;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.socin.treinamento.primeiro.enums.Naipe;
+import br.com.socin.treinamento.primeiro.enums.TipoCarta;
 import br.com.socin.treinamento.primeiro.model.Carta;
 import br.com.socin.treinamento.primeiro.model.Jogador;
 import br.com.socin.treinamento.primeiro.model.Jogo;
@@ -24,12 +29,24 @@ public class CartaServiceImpl implements CartaService {
 
   @Override
   public Map<Naipe, List<Carta>> ordernarCartasPorNaipe() {
-    return null;
+    List<Carta> cartas = cartaRepository.findAll();
+    return cartas.stream().filter(carta -> !carta.getTipoCarta().equals(TipoCarta.JOKER))
+        .collect(Collectors.groupingBy(carta -> Naipe.find(carta.getNaipe()).get()));
   }
 
   @Override
   public List<Carta> entregaCartasParaJogador(Jogador jogador, Jogo jogo) {
-    return null;
+    List<Carta> cartas = cartaRepository.findAll();
+
+    Collection<Jogador> jogadores = jogo.getJogadores().values();
+    List<Carta> cartasDosOutrosJogadores = jogadores.stream().filter(jog -> !jog.getId()
+        .equals(jogador.getId())).flatMap(jog -> jog.getCartas().stream()).collect(Collectors.toList());
+
+    Collections.shuffle(cartas);
+    return cartas.stream().filter(carta -> !cartasDosOutrosJogadores.stream()
+        .filter(cartaDoOutroJogador -> cartaDoOutroJogador.getNaipe().equals(carta.getNaipe())
+        && cartaDoOutroJogador.getTipoCarta().equals(carta.getTipoCarta())).findFirst().isPresent())
+        .limit(jogo.getJogo().getNumeroDeCartas()).collect(Collectors.toList());
   }
 
 }
--- src/main/java/br/com/socin/treinamento/primeiro/service/impl/JogoServiceImpl.java
+++ src/main/java/br/com/socin/treinamento/primeiro/service/impl/JogoServiceImpl.java
@@ -1,11 +1,19 @@
 package br.com.socin.treinamento.primeiro.service.impl;
 
+import static br.com.socin.treinamento.util.LambdaExceptionUtil.rethrowFunction;
+
 import java.util.List;
+import java.util.Optional;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import br.com.socin.treinamento.primeiro.enums.TipoDeJogo;
+import br.com.socin.treinamento.primeiro.model.Jogador;
 import br.com.socin.treinamento.primeiro.model.Jogo;
 import br.com.socin.treinamento.primeiro.repository.JogoRepository;
 import br.com.socin.treinamento.primeiro.service.CartaService;
@@ -32,12 +40,44 @@ public class JogoServiceImpl implements JogoService {
 
   @Override
   public Jogo iniciarJogo(List<Integer> jogadores, List<Integer> ordem, TipoDeJogo tipoJogo) {
-    return null;
+    final List<Integer> jogadoresProibidos =
+        jogoRepository.findAll().stream().filter(jogo -> !jogo.getEncerrou())
+            .flatMap(jogo -> jogo.getJogadores().values().stream().map(Jogador::getId))
+            .collect(Collectors.toList());
+
+    final List<Jogador> jogadoresMapeados =
+        jogadores.stream().filter(jogador -> !jogadoresProibidos.contains(jogador))
+            .map(rethrowFunction(idDoJogador -> jogadorService.buscarJogador(idDoJogador)))
+            .collect(Collectors.toList());
+
+    Assert.isTrue(jogadoresMapeados.size() == ordem.size(), TAMANHO_LISTA_IGUAL);
+
+    Assert.isTrue(jogadoresMapeados.size() <= tipoJogo.getLimiteJogadores(),
+        O_LIMITE_DO_TIPO_JOGO_DEVE_SER_MAIOR_OU_IGUAL);
+
+    final TreeMap<Integer, Jogador> ordemDosJogadores = IntStream.range(0, jogadoresMapeados.size()).boxed()
+        .collect(Collectors.toMap(ordem::get, jogadoresMapeados::get, (chave1, chave2) -> {
+          throw new IllegalStateException(String.format("Chave Duplicada! %s", chave1));
+        }, TreeMap::new));
+
+    Jogo jogo =
+        Jogo.builder().jogadores(ordemDosJogadores).encerrou(Boolean.FALSE).jogo(tipoJogo).build();
+
+    jogo = salvar(jogo);
+    return jogo;
   }
 
   @Override
   public Jogo distribuiCartas(Integer idJogo) {
-    return null;
+
+    Optional<Jogo> jogo = jogoRepository.findById(idJogo);
+    Assert.isTrue(jogo.isPresent(), O_JOGO_DEVE_EXISTIR);
+    Jogo jogoEncontrado = jogo.get();
+    Assert.isTrue(!jogoEncontrado.getEncerrou(), O_JOGO_NÃO_PODE_ESTAR_ENCERRADO);
+    jogoEncontrado.getJogadores().values().forEach(jogador -> jogador
+        .setCartas(cartaService.entregaCartasParaJogador(jogador, jogoEncontrado)));
+    salvar(jogoEncontrado);
+    return jogoEncontrado;
   }
 
   @Override
